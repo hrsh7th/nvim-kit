769b2edd0dd40ce7fe8a45e794bebdf2c0f352a9
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2022-10-02 20:53:53 +0900

    Fix for latest nvim

--- lua/___plugin_name___/kit/Vim/Syntax.lua
+++ lua/___plugin_name___/kit/Vim/Syntax.lua
@@ -28,7 +28,7 @@ end
 ---@return string[]
 function Syntax.get_treesitter_syntax_groups(cursor)
   local groups = {}
-  for _, capture in ipairs(vim.treesitter.get_captures_at_position(0, cursor[1], cursor[2])) do
+  for _, capture in ipairs(vim.treesitter.get_captures_at_pos(0, cursor[1], cursor[2])) do
     table.insert(groups, ('@%s'):format(capture.capture))
   end
   return groups
