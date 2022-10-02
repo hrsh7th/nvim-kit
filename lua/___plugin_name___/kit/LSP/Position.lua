96583eb4f66760b947dfb7631efcb66ea18cacce
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2022-10-02 21:23:30 +0900

    Improve LSP.Position

--- lua/___plugin_name___/kit/LSP/Position.lua
+++ lua/___plugin_name___/kit/LSP/Position.lua
@@ -12,24 +12,65 @@ Position.Encoding.UTF8 = 'utf8'
 Position.Encoding.UTF16 = 'utf16'
 Position.Encoding.UTF32 = 'utf32'
 
----Convert position to utf8 from specified encoding.
----@param position ___plugin_name___.kit.LSP.Position
----@param encoding? ___plugin_name___.kit.LSP.PositionEncoding
----@param text string
-function Position.to_utf8(position, encoding, text)
-  encoding = encoding or Position.Encoding.UTF16
+---Create cursor position.
+function Position.cursor(encoding)
+end
 
-  if encoding == Position.Encoding.UTF8 then
+---Convert position to utf8 from specified encoding.
+---@param text string
+---@param position ___plugin_name___.kit.LSP.Position
+---@param from_encoding? ___plugin_name___.kit.LSP.PositionEncoding
+---@return ___plugin_name___.kit.LSP.Position
+function Position.to_utf8(text, position, from_encoding)
+  from_encoding = from_encoding or Position.Encoding.UTF16
+
+  if from_encoding == Position.Encoding.UTF8 then
     return position
   end
 
   local ok, byteindex = pcall(function()
-    return vim.str_byteindex(text, position.character, encoding == Position.Encoding.UTF16)
+    return vim.str_byteindex(text, position.character, from_encoding == Position.Encoding.UTF16)
   end)
   if not ok then
     return position
   end
   return { line = position.line, character = byteindex }
+end
+
+---Convert position to utf16 from specified encoding.
+---@param text string
+---@param position ___plugin_name___.kit.LSP.Position
+---@param from_encoding? ___plugin_name___.kit.LSP.PositionEncoding
+---@return ___plugin_name___.kit.LSP.Position
+function Position.to_utf16(text, position, from_encoding)
+  local utf8 = Position.to_utf8(text, position, from_encoding)
+  for index = utf8.character, 0, -1 do
+    local ok, utf16index = pcall(function()
+      return select(2, vim.str_utfindex(text, index))
+    end)
+    if ok then
+      return { line = utf8.line, character = utf16index }
+    end
+  end
+  return position
+end
+
+---Convert position to utf32 from specified encoding.
+---@param text string
+---@param position ___plugin_name___.kit.LSP.Position
+---@param from_encoding? ___plugin_name___.kit.LSP.PositionEncoding
+---@return ___plugin_name___.kit.LSP.Position
+function Position.to_utf32(text, position, from_encoding)
+  local utf8 = Position.to_utf8(text, position, from_encoding)
+  for index = utf8.character, 0, -1 do
+    local ok, utf32index = pcall(function()
+      return select(1, vim.str_utfindex(text, index))
+    end)
+    if ok then
+      return { line = utf8.line, character = utf32index }
+    end
+  end
+  return position
 end
 
 return Position
