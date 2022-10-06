630b3a4c0de509b11d7253456e46d3ed0eab7477
uga-rosa <uga6603@gmail.com>
2022-10-06 22:41:02 +0900

    Use nvim_create_user_command to support for no argument

--- plugin/kit.lua
+++ plugin/kit.lua
@@ -3,7 +3,10 @@ if vim.g.loaded_kit then
 end
 vim.g.loaded_kit = true
 
-vim.cmd([[
-  command! -bang -nargs=* KitInstall lua require('kit.install')('<bang>' == '!', <f-args>)
-]])
-
+vim.api.nvim_create_user_command("KitInstall", function(opt)
+  local install_path, plugin_name = unpack(opt.fargs)
+  require("kit.install")(opt.bang, install_path, plugin_name)
+end, {
+  bang = true,
+  nargs = "*",
+})
