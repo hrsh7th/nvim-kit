5a3e9253ae46a32c97c4e490b7d071fdeaa5f256
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2023-01-07 19:15:05 +0900

    Add complex keymap test

--- lua/___kit___/kit/Vim/Keymap.spec.lua
+++ lua/___kit___/kit/Vim/Keymap.spec.lua
@@ -1,3 +1,4 @@
+local Async = require('___kit___.kit.Async')
 local Keymap = require('___kit___.kit.Vim.Keymap')
 
 describe('kit.Vim.Keymap', function()
@@ -6,6 +7,24 @@ describe('kit.Vim.Keymap', function()
       enew!
       set noswapfile
     ]])
+  end)
+
+  it('should send multiple keys in sequence', function()
+    vim.keymap.set('i', '(', Async.async(function()
+      Keymap.send('(', 'in'):await()
+      assert.equals('[(', vim.api.nvim_get_current_line())
+
+      Keymap.send(')', 'in'):await()
+      assert.equals('[()', vim.api.nvim_get_current_line())
+
+      Keymap.send(Keymap.termcodes('<Left>a<Right>'), 'in'):await()
+      assert.equals('[(a)', vim.api.nvim_get_current_line())
+    end))
+    Keymap.spec(function()
+      Keymap.send('i', 'in'):await()
+      Keymap.send('[(]', 'i'):await()
+      assert.equals('[(a)]', vim.api.nvim_get_current_line())
+    end)
   end)
 
   it('should work with async-await and exceptions', function()
@@ -17,7 +36,7 @@ describe('kit.Vim.Keymap', function()
     end)
     assert.are_not.equals(
       string.match(
-        err--[[@as string]],
+        err--[[@as string]] ,
         'error$'
       ),
       nil
